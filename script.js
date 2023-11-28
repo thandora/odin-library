a3daa1b3075703ffee5b7366613b458c2fcb96c4
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2023-11-29 07:16:55 +0800

    Add validation on book form

--- script.js
+++ script.js
@@ -12,17 +12,51 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(book);
 }
 
+const bookForm = document.querySelector("form");
 const dialog = document.querySelector("dialog");
+
 const btnAdd = document.querySelector(".btn-add-book");
+const btnAddConfirm = document.querySelector("#btn-add-confirm");
 const btnCancel = document.querySelector(".btn-cancel");
+
+// Helper functions
+function formOpen() {
+  dialog.showModal();
+}
 
 function formClose() {
   dialog.close();
 }
 
-function formOpen() {
-  dialog.showModal();
+function validateInputs(form) {
+  // Validates all input from `form` that has the `required` attribute.
+  // Returns a `true` if all valid, `false` otherwise.
+
+  // This is not best practice; implement server-side validation for
+  // more serious projects.
+
+  // Convert nodelist to array to allow .every() method
+  const requiredFields = Array.from(form.querySelectorAll("input[required]"));
+
+  return requiredFields.every((x) => {
+    return x.checkValidity();
+  });
 }
+
+btnAddConfirm.addEventListener("click", () => {
+  if (validateInputs(bookForm)) {
+    let title = bookForm.elements["title"].value;
+    let author = bookForm.elements["author"].value;
+    let pages = bookForm.elements["pages"].value;
+    let read = bookForm.elements["read"].checked;
+
+    console.log(title, author, pages, read);
+  }
+});
+
+bookForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
 
 btnAdd.addEventListener("click", formOpen);
 btnCancel.addEventListener("click", formClose);
